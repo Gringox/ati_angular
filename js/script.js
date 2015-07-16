7e51d254d4b36e7e686644ae07069ce34b1965c1
Innovation Workshop <inwork@MacBook-Pro.local>
2015-07-16 00:41:54 -0430

    Some changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-	var host = 'http://testserver.com:3000'
+var host = 'http://testserver.com:3000'
 
-	// create the module and name it atiApp
-	var atiApp = angular.module('atiApp', ['ngRoute', 'LocalStorageModule']);
+// create the module and name it atiApp
+var atiApp = angular.module('atiApp', ['ngRoute', 'LocalStorageModule']);
 
-	// configure our routes
-	atiApp.config(function($routeProvider) {
-		$routeProvider
+// configure our routes
+atiApp.config(function($routeProvider) {
+	$routeProvider
 
-			// route for the home page
-			.when('/', {
-				templateUrl: 'pages/home.html',
-				controller: 'mainController'
-			})
+		// route for the home page
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController'
+		})
 
-			// route for the package page
-			.when('/package', {
-				templateUrl: 'pages/package.html',
-				controller: 'packageController'
-			})
+		// route for the package page
+		.when('/todos', {
+			templateUrl: 'pages/todos.html',
+			controller: 'todosController'
+		})
 
-			// route for the user page
-			.when('/user', {
-				templateUrl: 'pages/user.html',
-				controller: 'userController'
-			})
-	});
+		// route for the user page
+		.when('/user', {
+			templateUrl: 'pages/user.html',
+			controller: 'userController'
+		})
+});
 
 // create the controller and inject Angular's $scope
 atiApp.controller('mainController', function($scope, $http, $location, localStorageService) {
@@ -37,24 +37,28 @@ atiApp.controller('mainController', function($scope, $http, $location, localStor
 		var that = this;
 		this.error = false;
 
-		$http.post(host+'/login', {
-			'email': that.user.email,
-			'password': that.user.password
-		})
-    .success(function(data) {
-      localStorageService.set('login_token', data.login_token);
-      localStorageService.set('id', data.id);
-    });
+		// POST Example
+		// $http.post(host+'/login', {
+		// 	'email': that.user.email,
+		// 	'password': that.user.password
+		// })
+    // .success(function(data) {
+    //   localStorageService.set('login_token', data.login_token);
+    //   localStorageService.set('id', data.id);
+    // });
 
-    this.user.email = '';
-    this.user.password = '';
+		localStorageService.set('login_token', '123456');
+    localStorageService.set('id', '1');
+
 	};
 
 	$scope.signOut = function() {
-		$http.delete(host+'/login/'+localStorageService.get('login_token'))
-		.success(function(data) {
-			    console.log(data);
-		});
+
+		// DELETE Example
+		// $http.delete(host+'/login/'+localStorageService.get('login_token'))
+		// .success(function(data) {
+		// 	    console.log(data);
+		// });
 
 		localStorageService.remove("login_token");
 		localStorageService.remove("id");
@@ -66,15 +70,12 @@ atiApp.controller('mainController', function($scope, $http, $location, localStor
 	$scope.message = 'Everyone come and see how good I look!';
 });
 
-atiApp.controller('packageController', function($scope, $http, localStorageService) {
+atiApp.controller('todosController', function($scope, $http, localStorageService) {
 
-	$http.get(host+'/users/'+localStorageService.get('id'), {
-		headers: {
-			'Token': localStorageService.get('login_token')
-		}
-	})
+	$http.get(host+'/todos')
 	.success(function(data){
-		$scope.packages = data.package;
+		console.log(data);
+		$scope.todos = data;
 	});
 
 	$scope.message = "Hi I'm a Package page";
@@ -82,14 +83,7 @@ atiApp.controller('packageController', function($scope, $http, localStorageServi
 
 atiApp.controller('userController', function($scope, $http, localStorageService) {
 
-	$http.get(host+'/users/'+localStorageService.get('id'), {
-		headers: {
-			'Token': localStorageService.get('login_token')
-		}
-	})
-	.success(function(data){
-		$scope.user = data;
-	});
+	$scope.user = {name: 'Pedro', lastname: 'Valdivieso'};
 
 	$scope.message = 'Develop by somebody';
 });
